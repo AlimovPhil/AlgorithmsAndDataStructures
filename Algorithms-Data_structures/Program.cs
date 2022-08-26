5f0996069467fcf9dc8c19d997f6496b611ae6da
AlimovPhil <retrom.rus@gmail.com>
2022-08-26 11:56:47 +0300

    Задания 2,3 выполнены. Асимптотическая сложность, Число Фибонначи.

--- Algorithms-Data_structures/Program.cs
+++ Algorithms-Data_structures/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Algorithms_Data_structures;
 
-var task1 = new Task1();
-task1.TestFunc();
+//var task1 = new Task1();
+//task1.TestFunc();
+
+var task3 = new Task3();
+task3.Test();
 
 Console.WriteLine("Завершение программы...");
 Console.ReadLine();
