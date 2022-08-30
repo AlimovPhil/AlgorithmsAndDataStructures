cd97e0419e4f6d4a73425a4466b0634fa5c18022
AlimovPhil <retrom.rus@gmail.com>
2022-08-30 14:12:26 +0300

    Задание 1. Двусвязный список.

---
+++ Algorithms-Data_structures/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms_Data_structures;
+
+public class BinarySearch
+{
+    public static int BinarySearch(int[] inputArray, int searchValue)
+    {
+        int min = 0;
+        int max = inputArray.Length - 1;
+        while (min <= max)
+        {
+            int mid = (min + max) / 2;
+            if (searchValue == inputArray[mid])
+            {
+                return mid;
+            }
+            else if (searchValue < inputArray[mid])
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
--- Algorithms-Data_structures/Program.cs
+++ Algorithms-Data_structures/Program.cs
@@ -1,4 +1,37 @@
-﻿
+﻿using Algorithms_Data_structures;
+
+
+var List1 = new LinkedList();
+
+List1.AddNode(11);
+List1.AddNode(22);
+List1.AddNode(33);
+List1.AddNode(44);
+List1.AddNode(55);
+
+Console.WriteLine("List1 список узлов:");
+List1.PrintAllNodes();
+Console.WriteLine($"Количество узлов: {List1.GetCount()}\n");
+
+Console.WriteLine("Добавление нового узла (66) после узла со значением (11)\n");
+var requestNode = List1.FindNode(11);
+List1.AddNodeAfter(requestNode, 66);
+
+Console.WriteLine("List1 список узлов после добавления:");
+List1.PrintAllNodes();
+Console.WriteLine($"Количество узлов: {List1.GetCount()}\n");
+
+Console.WriteLine("Удаление узла со значением (11) из списка:");
+List1.RemoveNode(requestNode);
+List1.PrintAllNodes();
+Console.WriteLine($"Количество узлов: {List1.GetCount()}\n");
+
+Console.WriteLine("Удаление узла с индексом (2) из списка:");
+List1.RemoveNode(2);
+List1.PrintAllNodes();
+Console.WriteLine($"Количество узлов: {List1.GetCount()}\n");
+
+
 
 
 Console.WriteLine("Завершение работы программы...");
